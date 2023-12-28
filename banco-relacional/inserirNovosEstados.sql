b24cfb159f47fdc56f85c3f67f91cb6dd77347ce
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-12-28 20:02:06 -0300

    inserir dados com ID

---
+++ banco-relacional/inserirNovosEstados.sql
@@ -0,0 +1,8 @@
+insert into estados(id, nome, sigla, regiao, populacao)
+values (100, 'Novo', 'NV', 'Sul', 2.54);
+
+insert into estados( nome, sigla, regiao, populacao)
+values ('Mais Novo', 'MN', 'Norte', 2.85);
+
+
+select * from `estados`
