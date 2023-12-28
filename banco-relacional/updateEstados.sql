6799b82093e6c665ba0fe7880fa5d5486fc2b94c
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-12-28 19:52:59 -0300

    update de dados com SQL

---
+++ banco-relacional/updateEstados.sql
@@ -0,0 +1,12 @@
+update `estados`
+set nome = 'Maranhão'
+WHERE sigla = 'MA'
+
+SELECT nome FROM `estados` WHERE sigla = 'MA'
+
+update `estados`
+set nome = 'Paraná',
+    populacao = 11.32
+WHERE sigla = 'PR';
+
+SELECT nome, sigla, populacao FROM `estados` WHERE sigla = 'PR'
