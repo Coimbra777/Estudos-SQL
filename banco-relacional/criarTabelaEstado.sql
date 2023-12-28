f0970bd9bd289c99ccee8c7113c226e4f0eb1aa0
Gabriel Coimbra <gabrielfelipe.coimbra@gmail.com>
2023-12-27 23:17:18 -0300

    criando tabela

---
+++ banco-relacional/criarTabelaEstado.sql
@@ -0,0 +1,12 @@
+-- Criadno tabela Estado
+
+create table estados (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+  nome VARCHAR(45) NOT NULL,
+  sigla CHAR(2) NOT NULL,
+  regiao ENUM('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') NOT NULL,  
+  populacao DECIMAL(5,2) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE KEY (nome),
+  UNIQUE KEY (sigla)
+);
